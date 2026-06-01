fcd3f863151b15972747fbbee9f3d2a448f92fb0
mendsec <fabiomendes@mailfence.com>
2026-06-01 00:54:22 -0400

    Fix CI: enable CGO for go test -race

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 import json
 try:
-    with open('annotations.json', encoding='utf-16') as f:
+    with open('pr24_test_annotations.json', encoding='utf-16') as f:
         data = json.load(f)
     print(data)
 except Exception as e:
