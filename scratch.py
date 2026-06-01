e59c6b4cf620cbda6f5ea88c42d29226c4980f07
mendsec <fabiomendes@mailfence.com>
2026-06-01 01:05:15 -0400

    chore: remove old scratch files from root

--- scratch.py
+++
@@ -1,9 +0,0 @@
-import json
-try:
-    with open('pr24_test_annotations.json', encoding='utf-16') as f:
-        data = json.load(f)
-    print(data)
-except Exception as e:
-    print(e)
-except Exception as e:
-    print(e)
